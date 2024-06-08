c3a42ad87dea85bb3c75b08e5ddf808be8ab98a6
Mateusj2506 <mateus_jorge2002@outlook.com>
2024-06-08 13:35:27 -0300

    media de tres notas

--- exe16.py
+++ exe16.py
@@ -1,1 +1,19 @@
 
+nota1 = int(input("Digite a primeira nota: "))
+
+nota2 = int(input("Digite a segunda nota: "))
+
+nota3 = int(input("Digite a terceira nota: "))
+
+
+media = (nota1 + nota2 + nota3)/3
+
+if (media >= 7) == (media < 10):
+    print("Aprovado com: ", media)
+
+elif media < 7:
+    print("Reprovado com: ", media)
+
+else:
+    print("Aprovado com Distinção com media maior que 10")
+
